ef5cb85bdd350b9306544060a6f7e7e9491e6583
Michael <frogpp@msn.com>
2020-12-17 17:55:39 +0800

    Iframe

---
+++ FrogWebAPI/Controllers/IFrameController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrogWebAPI.Controllers
+{
+    public class IFrameController : Controller
+    {
+        public ActionResult Index()
+        {
+            switch (Request.QueryString["option"])
+            {
+                case "Swagger":
+                    ViewBag.PageOption = "Swagger";
+                    break;
+                default:
+                    break;
+            }
+            return View();
+        }
+    }
+}
